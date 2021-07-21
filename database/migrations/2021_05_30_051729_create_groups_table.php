e3b78724bb219c9198b60b87d5a67eb6eccd5e62
Lucas <lucas.sanner@codalia.fr>
2021-07-21 07:30:13 +0000

    Add migrations.

---
+++ database/migrations/2021_05_30_051729_create_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 80);
+            $table->tinyText('description')->nullable();
+            $table->char('access_level', 10);
+            $table->tinyInteger('role_level')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
