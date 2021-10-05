6c553111591d96c00892ef75ee3fa95047947405
Lucas <lucas.sanner@codalia.fr>
2021-10-05 04:57:51 +0000

    Adds missing tables.

---
+++ database/migrations/2021_08_25_075701_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80);
+            $table->string('slug', 80);
+            $table->text('content');
+            $table->tinyText('excerpt')->nullable();
+            $table->char('status', 12);
+            $table->unsignedBigInteger('main_cat_id')->nullable();
+            $table->char('access_level', 10);
+            $table->unsignedBigInteger('checked_out')->nullable();
+            $table->timestamp('checked_out_time')->nullable();
+            $table->unsignedBigInteger('owned_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
